f6df42ba7a845107847c5452c31b01fde4da7395
Radosław Ganczarek <radoslaw@ganczarek.in>
2022-02-26 23:17:17 +0100

    Add mypy check

--- backend/holistic/asgi.py
+++ backend/holistic/asgi.py
@@ -1,23 +1,24 @@
-import uvicorn
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.routing import Route
+from uvicorn import run
 
 
-async def homepage(request):
-    return JSONResponse({"message": "hello world"})
+async def homepage(request: Request) -> JSONResponse:
+    response: dict[str, str] = {"message": "hello world"}
+    return JSONResponse(response)
 
 
+origins: list[str] = ["http://localhost:3000"]
 app = Starlette(
     debug=True,
-    routes=[
-        Route("/", homepage),
-    ],
-    middleware=[Middleware(CORSMiddleware, allow_origins=["http://localhost:3000"])],
+    routes=[Route("/", homepage)],
+    middleware=[Middleware(CORSMiddleware, allow_origins=origins)],
 )
 
 
-def start():
-    uvicorn.run("holistic.asgi:app", host="0.0.0.0", port=8000, reload=True)
+def start() -> None:
+    run(app="holistic.asgi:app", host="0.0.0.0", port=8000, reload=True)
